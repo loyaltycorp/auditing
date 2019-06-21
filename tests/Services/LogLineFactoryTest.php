f6c75af911280a93ff05ebcf149a0d8ba74e1e97
Tim Nagel <tim@nagel.com.au>
2019-06-21 14:19:35 +1000

    Fix LogLine request supporting empty requests (#11)
    * Fix LogLine request supporting empty requests
    
    * Ketan messing up

--- tests/Services/LogLineFactoryTest.php
+++ tests/Services/LogLineFactoryTest.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 namespace Tests\LoyaltyCorp\Auditing\Services;
 
 use EoneoPay\Utils\DateTime;
+use GuzzleHttp\Psr7\BufferStream;
 use LoyaltyCorp\Auditing\Services\LogLineFactory;
 use Tests\LoyaltyCorp\Auditing\Stubs\Services\LogLineFactory\RequestStub;
 use Tests\LoyaltyCorp\Auditing\Stubs\Services\LogLineFactory\ResponseStub;
 use Tests\LoyaltyCorp\Auditing\TestCase;
+use Zend\Diactoros\Request;
 
 /**
  * @covers \LoyaltyCorp\Auditing\Services\LogLineFactory
@@ -34,6 +36,32 @@ class LogLineFactoryTest extends TestCase
 
         self::assertSame('127.0.0.1', $dto->getClientIp());
         self::assertSame('2019-05-05 12:12:12', $dto->getOccurredAt()->format('Y-m-d H:i:s'));
+        self::assertIsString($dto->getResponseData());
+    }
+
+    /**
+     * Test creating a log line DTO using the factory
+     *
+     * @return void
+     *
+     * @throws \EoneoPay\Utils\Exceptions\InvalidDateTimeStringException
+     */
+    public function testCreationEmptyRequestBecomesNull(): void
+    {
+        $request = new Request(null, null, new BufferStream());
+
+        $logLineFactory = $this->getInstance();
+
+        $dto = $logLineFactory->create(
+            '127.0.0.1',
+            new DateTime('2019-05-05 12:12:12'),
+            $request,
+            new ResponseStub()
+        );
+
+        self::assertSame('127.0.0.1', $dto->getClientIp());
+        self::assertSame('2019-05-05 12:12:12', $dto->getOccurredAt()->format('Y-m-d H:i:s'));
+        self::assertNull($dto->getRequestData());
         self::assertIsString($dto->getResponseData());
     }
 
